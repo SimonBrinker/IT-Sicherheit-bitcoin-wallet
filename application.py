d6920a377f73200edf2c41661e8ac6074a69066e
_c3ngiz_ <115454703+CengizhanOemuer@users.noreply.github.com>
2023-05-05 18:22:01 +0200

    Update application.py
    Bugfixes und tausend neue Bugs. 
    Mache morgen weiter, das zweite Fenster wird jetzt erzeugt, man kann es auch schließen um auf die Home-Seite zukommen usw.

--- application.py
+++ application.py
@@ -20,6 +20,7 @@ class Window1(customtkinter.CTk):
         # Window settings:
         self.geometry("1100x580")
         self.title("CryptoUI")
+        self.wallet_window = None
 
         ##################################################################################
         # Grid layout settings:
@@ -43,11 +44,11 @@ class Window1(customtkinter.CTk):
         self.server_label.grid(row=1, column=0, pady=(0, 5))
         self.server_mainnet_button = customtkinter.CTkButton(self.sidebar_frame,
                                                              text="BTC-Mainnet",
-                                                             command=self.switch_server())
+                                                             command=self.switch_server)
         self.server_mainnet_button.grid(row=3, column=0, pady=(0, 5))
         self.server_testnet_button = customtkinter.CTkButton(self.sidebar_frame,
                                                              text="BTC-Testnet",
-                                                             command=self.switch_server())
+                                                             command=self.switch_server)
         self.server_testnet_button.grid(row=4, column=0, padx=(10, 10), pady=(0, 15))
 
         # Appearance setting widgets:
@@ -102,7 +103,7 @@ class Window1(customtkinter.CTk):
                                                     text="Login",
                                                     width= 300,
                                                     command=self.login)
-        self.login_button.grid(row=8, column=2, padx=(15, 30), pady=(10, 30))
+        self.login_button.grid(row=8, column=2, padx=(30, 30), pady=(10, 30))
 
         ##################################################################################
         # Setting the default values:
@@ -111,20 +112,117 @@ class Window1(customtkinter.CTk):
         self.ui_scaling_optionmenu.set("100%")
 
     def switch_server(self):
-        #if self.login_label.cget("text") == "Login to BTC-Mainnet":
-            #self.login_label.configure(True, "Login to BTC-Testnet")
-        pass
+        login_label_text = self.login_label.cget("text")
+        if login_label_text == "Login to Mainnet-Server":
+            self.login_label.configure(True, text="Login to Testnet-Server")
+        elif login_label_text == "Login to Testnet-Server":
+            self.login_label.configure(True, text="Login to Mainnet-Server")
 
-    def switch_appearance(self, new_appearance_mode: str):
+    @staticmethod
+    def switch_appearance(new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
 
-    def switch_scaling(self, new_scaling : str):
+    @staticmethod
+    def switch_scaling(new_scaling : str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
 
     def login(self):
-        pass
+        # self.withdraw()
+        if self.wallet_window is None or not self.wallet_window.winfo_exists():
+            self.wallet_window = Window2()  # create window if its None or destroyed
+        else:
+            self.wallet_window.focus()  # if window exists focus it
+        print("Logged in!")
 
+
+class Window2(customtkinter.CTkToplevel):
+    def __init__(self):
+        super().__init__()
+
+        ##################################################################################
+        # Window settings:
+        self.geometry("1100x580")
+        self.title("Wallet")
+
+
+        ##################################################################################
+        # Grid layout settings:
+        self.grid_columnconfigure(0, weight=0)
+        self.grid_columnconfigure((1, 2), weight=1)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), weight=1)
+
+        ##################################################################################
+        # Creating sidebar:
+        self.sidebar_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=11, sticky="nsew")
+
+        # Creating Server-switch widgets:
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame,
+                                                 text="CryptoUI",
+                                                 font=customtkinter.CTkFont(size=30, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=35, pady=(20, 15))
+
+        # Appearance setting widgets:
+        self.appearance_label = customtkinter.CTkLabel(self.sidebar_frame,
+                                                       text="Appearance Settings",
+                                                       font=customtkinter.CTkFont(size=18, weight="bold"))
+        self.appearance_label.grid(row=1, column=0)
+        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame,
+                                                            text="Mode",
+                                                            font=customtkinter.CTkFont(size=16, weight="bold"))
+        self.appearance_mode_label.grid(row=2, column=0, pady=(0, 5))
+        self.appearance_mode_optionmenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                                      values=["Dark", "Light", "System"],
+                                                                      command=self.switch_appearance)
+        self.appearance_mode_optionmenu.grid(row=3, column=0, pady=(0, 15))
+        self.ui_scaling_label = customtkinter.CTkLabel(self.sidebar_frame,
+                                                       text="UI-Scaling",
+                                                       font=customtkinter.CTkFont(size=18, weight="bold"))
+        self.ui_scaling_label.grid(row=4, column=0, pady=(0, 5))
+        self.ui_scaling_optionmenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
+                                                                 values=["80%", "90%", "100%", "110%", "120%"],
+                                                                 command=self.switch_scaling)
+        self.ui_scaling_optionmenu.grid(row=5, column=0, pady=(0, 15))
+
+        # Using the blank space:
+        self.ad_bar_frame = customtkinter.CTkFrame(self.sidebar_frame, width=150, height=200)
+        self.ad_bar_frame.grid(row=6, column=0)
+        self.ad_label = customtkinter.CTkLabel(self.ad_bar_frame,
+                                               text="Here could be ur add!",
+                                               font=customtkinter.CTkFont(weight="bold"))
+        self.ad_label.grid(row=6, column=0, padx=(10, 10), pady=(85, 85))
+
+        # Creating logout widgets:
+        self.logout_label = customtkinter.CTkLabel(self.sidebar_frame,
+                                                   text="Switch User",
+                                                   font=customtkinter.CTkFont(size=18, weight="bold"))
+        self.logout_label.grid(row=7, column=0, pady=(25, 5))
+        self.logout_button = customtkinter.CTkButton(self.sidebar_frame,
+                                                     text="Logout!",
+                                                     command=self.logout)
+        self.logout_button.grid(row=8, column=0, sticky="s")
+
+        ##################################################################################
+        # Setting the default values:
+        customtkinter.set_appearance_mode("Dark")
+        self.appearance_mode_optionmenu.set("Dark")
+        self.ui_scaling_optionmenu.set("100%")
+
+    ##################################################################################
+
+    @staticmethod
+    def switch_appearance(new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    @staticmethod
+    def switch_scaling(new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+    def logout(self):
+        self.master.destroy()
+        Window1()
 
 def main():
     run = Application()
