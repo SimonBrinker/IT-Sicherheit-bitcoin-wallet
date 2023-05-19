2a9aae1c5ade0e33cf81ba81852659397e9e8211
Zamki <74539955+Zamk1@users.noreply.github.com>
2023-05-19 20:09:58 +0200

    added Method to get all Transactions linked to an adress

--- wallet_api.py
+++ wallet_api.py
@@ -7,5 +7,33 @@ useApi = False
 def get_wallet_balance(wallet:Wallet):
     if not useApi:
         return 0.0001
+    
     satoshis = blockcypher.get_total_balance(wallet.get_address_string(), coin_symbol=wallet.network.value, api_key=apiKey)
+    
     return blockcypher.satoshis_to_btc(satoshis)
+
+def get_transactions(wallet:Wallet):
+    if not useApi:
+        return None
+    
+    transactions = blockcypher.get_address_full(wallet.get_address_string(), coin_symbol=wallet.network.value, api_key=apiKey)
+
+    return transactions
+
+"""
+    for transaction in transactions['txs']:
+        print("Transaktionshash:", transaction['hash'])
+        print("Bestätigungen:", transaction['confirmations'])
+        print("Eingehende Transaktionen:")
+        for input_tx in transaction['inputs']:
+            print("  Von:", input_tx['addresses'][0])
+            print("  Wert:", input_tx['output_value'])
+        print("Ausgehende Transaktionen:")
+        for output_tx in transaction['outputs']:
+            print("  An:", output_tx['addresses'][0])
+            print("  Wert:", output_tx['value'])
+        if 'spent' in output_tx and output_tx['spent']:
+            print("  Ausgabe bereits verbraucht")
+        else:
+            print("  Ausgabe noch nicht verbraucht")
+        print("----------")"""
